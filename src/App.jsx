a85881196419471725dfe468844fe1f66e2df80c
Jasiuu2605 <98971547+Jasiuu2605@users.noreply.github.com>
2022-12-12 13:44:14 +0100

    setup projects boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+    return ( 
+        <header>Hello World</header>
+     );
+}
+ 
+export default App;
---
+++ webpack/production.js
@@ -0,0 +1,56 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[local]",
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(s(a|c)ss|css)$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash:6].css",
+      chunkFilename: "style.[contenthash:6].css",
+      
+    }),
+  ],
+};
