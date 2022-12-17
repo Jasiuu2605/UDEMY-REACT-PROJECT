65527ff6576b3825539aec64c64eab40af9c70aa
Jasiuu2605 <98971547+Jasiuu2605@users.noreply.github.com>
2022-12-17 19:17:04 +0100

    bugs fix

--- src/components/Courses/Subcomponents/Course.jsx
+++ src/components/Courses/Subcomponents/Course.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import bemCssModules from "bem-css-modules";
+import { StoreContext } from "../../../store/StoreProvider";
+import request from "../../../helpers/request";
+import { useHistory } from "react-router-dom";
 
-import bemCssModules from 'bem-css-modules'
+import { default as CourseStyle } from "./Course.module.scss";
 
-import { default as CourseStyle } from './Course.module.scss'
+const style = bemCssModules(CourseStyle);
 
-const style = bemCssModules(CourseStyle)
+const Course = ({ authors, img, price, title, id, isUserContext = false }) => {
+  const { user, setUser } = useContext(StoreContext);
+  const history = useHistory();
 
-const Course = ({ authors, img, price, title}) => {
+  const allAuthors = authors.join(", ");
 
-   const allAuthors = authors.join(', ');
+  const isUserLogged = Boolean(user);
 
-   return (
-     <li>
-       <article className={style()}>
-         <h3 className={style("title")}></h3>
-         <img className={style("image")} src={img} alt={title} />
-         <p className={style("price")}>{`Koszt kursu: ${price}zł`}</p>
-         <p className={style("authors")}>{`Autorzy kursu: ${allAuthors}`}</p>
-       </article>
-     </li>
-   );
-}
- 
+  const handleOnCLick = async () => {
+    try {
+      const { data, status } = await request.patch("/users", {
+        login: user.login,
+        courseId: id,
+      });
+
+      if (status === 202) {
+        setUser(data.user);
+        history.push("/my-courses");
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
+
+  return (
+    <li>
+      <article className={style()}>
+        <h3 className={style("title")}>{title}</h3>
+        <img className={style("image")} src={img} alt={title} />
+        <p className={style("price")}>{`Koszt kursu: ${price}zł`}</p>
+        <p className={style("authors")}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBuyButtonVisible && (
+          <button onClick={handleOnCLick}>Zakup ten kurs</button>
+        )}
+      </article>
+    </li>
+  );
+};
+
 export default Course;
